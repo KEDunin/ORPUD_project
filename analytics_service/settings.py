f342f368098f84498eee328b0f1c9a617a0c12f1
Amir Sharafeev <nail_shar@mail.ru>
2023-04-30 17:02:21 +0300

    ÑÐДоьÐбавленавление двуÐх таблицÐÐ

--- analytics_service/settings.py
+++ analytics_service/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'analytics_service.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '654321amir',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
--- web_analytics_service/models.py
+++ web_analytics_service/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+class Shop(models.Model):
+    name = models.CharField(max_length=50)
 
-# Create your models here.
+
+class Iphone(models.Model):
+    number = models.IntegerField()
+    model = models.CharField(max_length=30)
+    color = models.CharField(max_length=30)
+    memory = models.CharField(max_length=30)
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+
