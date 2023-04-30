31526884d80b86d59486d40f759a4645e240f6ce
Кирилл (админ) <kdunin@bk.ru>
2023-04-30 14:43:50 +0300

    Добавление нового функционала на главной странице main.html (выпадающее меню)

--- web_analytics_service/urls.py
+++ web_analytics_service/urls.py
@@ -5,5 +5,5 @@ from web_analytics_service.views import main_view, preview_view
 
 urlpatterns = [
     path('', preview_view),
-    path('main.html/', main_view)
+    path('main/', main_view, name='main')
 ]
