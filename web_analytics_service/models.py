c1f0cf7380c9b546734282351c6ed39eaa6fbac0
Кирилл (админ) <kdunin@bk.ru>
2023-05-22 21:23:51 +0300

    Добавление модели DateAdd

--- web_analytics_service/models.py
+++ web_analytics_service/models.py
@@ -5,11 +5,15 @@ class Shop(models.Model):
     name = models.CharField(max_length=50)
 
 
+class DateAdd(models.Model):
+    date = models.DateField()
+
+
 class Iphone(models.Model):
     model = models.CharField(max_length=50)
     color = models.CharField(max_length=30)
     memory = models.IntegerField()
     shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
     price = models.IntegerField()
-    date = models.DateTimeField()
+    date = models.ForeignKey(DateAdd, on_delete=models.CASCADE)
     link = models.CharField(max_length=1000)
