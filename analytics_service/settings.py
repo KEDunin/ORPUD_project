e6b76b634703081c737a131507957a7417bf039f
Кирилл (админ) <kdunin@bk.ru>
2023-05-22 14:33:17 +0300

    добавление поля date в модель Iphone

--- analytics_service/settings.py
+++ analytics_service/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        'timeout': 500,
+        'timeout': 5000,
     }
 }
 
--- web_analytics_service/models.py
+++ web_analytics_service/models.py
@@ -5,14 +5,10 @@ class Shop(models.Model):
     name = models.CharField(max_length=50)
 
 
-class Date(models.Model):
-    date_value = models.DateField()
-
-
 class Iphone(models.Model):
     model = models.CharField(max_length=50)
     color = models.CharField(max_length=30)
     memory = models.IntegerField()
     shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
     price = models.IntegerField()
-    date = models.ForeignKey(Date, on_delete=models.CASCADE)
+    date = models.DateTimeField()
